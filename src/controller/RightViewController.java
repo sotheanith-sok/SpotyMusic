026be77edfa80864d67ae53c04fc21c67bdc78a1
Brian <Bapowell00@gmail.com>
2018-09-04 10:37:46 -0700

    my view

--- src/controller/RightViewController.java
+++ src/controller/RightViewController.java
@@ -1,4 +1,4 @@
 package controller;
 
-public class RightViewControlller {
+public class RightViewController {
 }
--- src/controller/SplashUIController.java
+++ src/controller/SplashUIController.java
@@ -1,4 +1,12 @@
 package controller;
 
+import java.awt.*;
+
 public class SplashUIController {
+    @FXML
+    private TextField txtUser;
+
+    @FXML
+    private TextField txtPass;
+
 }
