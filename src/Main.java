0edfd91d3dc2526e54bbf3afa018b1162e044ad7
Brian <bapowell00@gmail.com>
2018-09-17 21:35:43 -0700

    Login context switching now working

--- src/Main.java
+++ src/Main.java
@@ -10,30 +10,28 @@ import ui.component.Router;
 public class Main extends Application {
 
     public static String mainID = "main";
-    public static String mainFile = "MainView.fxml";
+    public static String mainFile = "../../ui/view/MainView.fxml";
     public static String splashID = "splash";
-    public static String splashFile = "SplashUI.fxml";
+    public static String splashFile = "../../ui/view/SplashUI.fxml";
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
+    public void start(Stage primaryStage) {
 
         DataManager.getDataManager().init();
 
-        //Router mainRouter = new Router();
-        //mainRouter.loadView(mainID, mainFile);
-        //mainRouter.loadView(splashID, splashFile);
+        Router mainRouter = new Router();
+        mainRouter.loadView(mainID, mainFile);
+        mainRouter.loadView(splashID, splashFile);
 
-        //mainRouter.setView(splashID);
+        mainRouter.setView(splashID);
 
-        //System.out.println(DataManager.getDataManager().getCurrentLibrary().get().getSongs().size());
-        Parent root = FXMLLoader.load(getClass().getResource("ui/view/MainView.fxml"));
-
-        //Group root = new Group();
-        //root.getChildren().addAll(mainRouter);
+        Group root = new Group();
+        root.getChildren().addAll(mainRouter);
         Scene scene = new Scene(root, 800, 600);
 
 
         primaryStage.setTitle("Spusic");
+        primaryStage.setScene(scene);
         primaryStage.setScene(scene);
         primaryStage.show();
     }
