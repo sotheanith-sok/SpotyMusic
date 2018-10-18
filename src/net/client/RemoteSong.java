16a4b0f9bd07447e46f2da44a9a35dedad6f8bf8
NicholasUtz <nicholas.utz@student.csulb.edu>
2018-10-18 00:04:11 -0700

    Updated to use net/lib instead of net/connect

--- src/net/client/RemoteSong.java
+++ src/net/client/RemoteSong.java
@@ -1,19 +1,20 @@
 package net.client;
 
 import com.fasterxml.jackson.core.JsonFactory;
-import com.fasterxml.jackson.core.JsonGenerator;
 import connect.Library;
 import connect.Song;
 import net.common.Constants;
 import net.common.JsonField;
 import net.common.SimpleJsonWriter;
-import net.connect.Session;
+import net.lib.Socket;
 
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.UnsupportedAudioFileException;
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.SocketException;
+import java.net.SocketTimeoutException;
 import java.util.concurrent.*;
 
 public class RemoteSong implements Song {
@@ -73,11 +74,21 @@ public class RemoteSong implements Song {
         Thread t = new Thread(() -> {
             System.out.println("[RemoteSong][getStream] Requesting song stream");
 
-            Session session = this.library.getSession();
-            session.debug = true;
-            System.out.println("[RemoteSong][getStream] Opened Session");
+            Socket socket = null;
+            try {
+                socket = this.library.getConnection();
 
-            SimpleJsonWriter request = new SimpleJsonWriter(session);
+            } catch (SocketException e) {
+                System.err.println("[RemoteSong][getStream] SocketException while opening connection");
+                future.completeExceptionally(e);
+
+            } catch (SocketTimeoutException e) {
+                System.err.println("[RemoteSong][getStream] Timed out while trying to connect");
+                e.printStackTrace();
+            }
+            System.out.println("[RemoteSong][getStream] Opened Socket");
+
+            SimpleJsonWriter request = new SimpleJsonWriter(socket, false);
             JsonField.ObjectField packet = JsonField.emptyObject();
             packet.setProperty(Constants.REQUEST_TYPE_PROPERTY, Constants.REQUEST_STREAM_SONG);
             packet.setProperty("id", this.getId());
@@ -85,10 +96,10 @@ public class RemoteSong implements Song {
             request.complete();
             library.taskManager.submit(request);
 
-            System.out.println("[RemoteSong][getStream] Request sending");
+            System.out.println("[RemoteSong][getStream] Request waitingAck");
             System.out.println("[RemoteSong][getStream] Waiting for response data");
 
-            InputStream in = session.inputStream();
+            InputStream in = socket.inputStream();
             try {
                 int i = 0;
                 do {
@@ -101,9 +112,9 @@ public class RemoteSong implements Song {
                     }
 
                     i++;
-                } while (in.available() < Constants.MIN_BUFFERED_DATA && session.isInputOpened() && i < 100);
+                } while (in.available() < Constants.MIN_BUFFERED_DATA && !socket.isReceiveClosed() && i < 100);
                 if (i == 100) {
-                    session.close();
+                    socket.close();
                     System.err.println("[RemoteSong][getStream] Request timed out");
                     future.completeExceptionally(new TimeoutException("Request for song stream timed out"));
                     return;
@@ -114,23 +125,25 @@ public class RemoteSong implements Song {
                 return;
             }
 
-            if (!session.isInputOpened()) {
+            if (socket.isReceiveClosed()) {
                 future.completeExceptionally(new IOException("Connection closed unexpectedly"));
                 return;
             }
 
             try {
                 future.complete(AudioSystem.getAudioInputStream(in));
+                System.out.println("[RemoteSong][getStream] It worked!");
 
             } catch (UnsupportedAudioFileException e) {
                 System.err.println("[RemoteSong][getStream] Audio stream format not supported");
+                socket.close();
                 future.completeExceptionally(e);
 
             } catch (IOException e) {
                 System.err.println("[RemoteSong][getStream] IOException while creating AudioInputStream");
+                socket.close();
                 future.completeExceptionally(e);
             }
-
         });
         t.setDaemon(true);
         t.setName("[RemoteSong][SongStreamRequester]");
