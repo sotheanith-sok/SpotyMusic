df9931458aa28e7ad469086dbff2c3dbbb8bdd2a
NicholasUtz <nicholas.utz@student.csulb.edu>
2018-10-18 00:01:16 -0700

    Added support for mark() in BufferConsumer. Fixed a bunch of really broken stuff.

--- src/utils/StreamBuffer.java
+++ src/utils/StreamBuffer.java
@@ -14,6 +14,11 @@ public class StreamBuffer {
     protected int head;
     protected int tail;
 
+    protected int mark;
+    protected int readLimit;
+    protected int readSinceMark;
+    protected int readSinceReset;
+
     protected AtomicBoolean readOpened;
     protected AtomicBoolean writeOpened;
 
@@ -26,6 +31,10 @@ public class StreamBuffer {
         this.lock = new Object();
         this.head = 0;
         this.tail = 0;
+        this.mark = Integer.MAX_VALUE;
+        this.readLimit = 0;
+        this.readSinceMark = 0;
+        this.readSinceReset = 0;
 
         this.readOpened = new AtomicBoolean(true);
         this.writeOpened = new AtomicBoolean(true);
@@ -42,16 +51,32 @@ public class StreamBuffer {
         return this.source;
     }
 
+    public boolean isWriteOpened() {
+        return this.writeOpened.get() && this.readOpened.get();
+    }
+
+    public boolean isReadOpened() {
+        return this.available() > 0 || this.writeOpened.get();
+    }
+
     public int capacity() {
         synchronized (lock) {
-            if (tail == head) {
-                return capacity - 1;
-
-            } else if (tail < head) {
-                return capacity - (head - ((tail + 1) % capacity));
+            int effectiveTail;
+            if (mark < capacity) {
+                effectiveTail = mark < tail ? mark : tail;
 
             } else {
-                return ((tail + 1) % capacity) - head;
+                effectiveTail = tail;
+            }
+
+            if (effectiveTail == head) {
+                return capacity - 1;
+
+            } else if (effectiveTail < head) {
+                return capacity - (head - ((effectiveTail + 1) % capacity));
+
+            } else {
+                return ((effectiveTail + 1) % capacity) - head;
             }
         }
     }
@@ -67,11 +92,10 @@ public class StreamBuffer {
             } else {
                 return 0;
             }
-
         }
     }
-
 }
+
 class BufferConsumer extends InputStream {
 
     private final StreamBuffer buffer;
@@ -86,8 +110,36 @@ class BufferConsumer extends InputStream {
     }
 
     @Override
+    public boolean markSupported() {
+        return true;
+    }
+
+    @Override
+    public void mark(int readLimit) {
+        synchronized (buffer.lock) {
+            buffer.mark = buffer.tail;
+            buffer.readLimit = readLimit;
+            buffer.readSinceMark = 0;
+            System.out.println("[StreamBuffer][BufferConsumer] BufferConsumer mark set with readLimit of " + readLimit);
+        }
+    }
+
+    @Override
+    public void reset() throws IOException {
+        synchronized (buffer.lock) {
+            if (buffer.mark > buffer.capacity) throw new IOException("Cannot reset to invalid mark");
+            buffer.tail = buffer.mark;
+            System.out.println("[StreamBuffer][BufferConsumer][reset] BufferConsumer mark was reset after reading " + buffer.readSinceMark + " bytes");
+            buffer.readSinceReset = 0;
+        }
+    }
+
+    @Override
     public int read() throws IOException {
-        if (!buffer.readOpened.get()) return -1;
+        if (!buffer.readOpened.get()) {
+            System.out.println("[StreamBuffer][BufferConsumer][read] Attempt to read closed buffer");
+            return -1;
+        }
         synchronized (buffer.lock) {
             if (available() == 0 && buffer.writeOpened.get()) {
                 try {
@@ -101,13 +153,71 @@ class BufferConsumer extends InputStream {
             if (available() > 0) {
                 int d = buffer.buffer[buffer.tail];
                 buffer.tail++;
+                buffer.readSinceReset++;
                 buffer.tail %= buffer.capacity;
+
+                if (buffer.mark < buffer.capacity) {
+                    if (buffer.readSinceMark > buffer.readLimit) {
+                        buffer.mark = Integer.MAX_VALUE;
+                    }
+                }
+
+                if (d == -1) System.err.println("[StreamBuffer][BufferConsumer][read] Read -1 from byte array. Thats not possible.");
+                //System.out.println("[StreamBuffer][BufferConsumer][read] read byte: " + d);
                 buffer.lock.notifyAll();
                 return d;
 
             } else {
+                System.out.println("[StreamBuffer][BufferConsumer][read] End of input after reading " + buffer.readSinceReset + " bytes since reset");
+                buffer.lock.notifyAll();
                 return -1;
             }
+        }
+    }
+
+    @Override
+    public int read(byte[] b, int off, int len) {
+        if (!buffer.readOpened.get()) {
+            System.err.println("[StreamBuffer][BufferConsumer] Attempt to read closed buffer");
+            return -1;
+        }
+        synchronized (buffer.lock) {
+            int toRead = Math.min(this.available(), len);
+
+            while (toRead == 0) {
+                try {
+                    buffer.lock.wait();
+                } catch (InterruptedException e) {}
+
+                if (!buffer.readOpened.get() && buffer.available() == 0) {
+                    //System.err.println("[SteamBuffer][BufferConsumer][read] StreamBuffer Consumer closed while waiting for input");
+                    return -1;
+                }
+
+                if (buffer.available() == 0 && !buffer.writeOpened.get()) {
+                    //System.err.println("[StreamBuffer][BufferConsumer][read] StreamBuffer Producer closed while waiting for input");
+                    return -1;
+                }
+
+                toRead = Math.min(this.available(), len);
+            }
+
+            for (int i = 0; i < toRead; i++) {
+                b[off + i] = buffer.buffer[buffer.tail];
+                buffer.tail++;
+                buffer.tail %= buffer.capacity;
+                buffer.readSinceReset++;
+                buffer.readSinceMark++;
+            }
+
+            if (buffer.mark < buffer.capacity) {
+                if (buffer.readSinceMark > buffer.readLimit) {
+                    buffer.mark = Integer.MAX_VALUE;
+                }
+            }
+
+            buffer.lock.notifyAll();
+            return toRead;
         }
     }
 
@@ -118,7 +228,9 @@ class BufferConsumer extends InputStream {
             buffer.lock.notifyAll();
         }
     }
+
 }
+
 class BufferProvider extends OutputStream {
 
     private final StreamBuffer buffer;
@@ -129,6 +241,7 @@ class BufferProvider extends OutputStream {
 
     @Override
     public void write(int b) throws IOException {
+        if (!buffer.writeOpened.get()) throw new IOException("Buffer is closed to writing");
         synchronized (buffer.lock) {
             if (buffer.capacity() <= 0 && buffer.readOpened.get()) {
                 try {
@@ -159,14 +272,28 @@ class BufferProvider extends OutputStream {
 
     @Override
     public void write(byte[] b, int off, int len) throws IOException {
+        if (!buffer.writeOpened.get()) throw new IOException("Buffer is closed to writing");
+        synchronized (this.buffer.lock) {
+            int written = 0;
+            do {
+                int toWrite = Math.min(len - written, buffer.capacity());
+                for (; written < toWrite; written++) {
+                    buffer.buffer[buffer.head] = b[off + written];
+                    buffer.head++;
+                    buffer.head %= buffer.capacity;
+                }
 
-       synchronized (this.buffer.lock) {
-          for (int i = 0; i < len; i++) {
-             write(b[off + i]);
-          }
-       }
+                if (written < len) {
+                    try {
+                        this.buffer.lock.wait();
+                    } catch (InterruptedException e) {}
+                }
 
-       System.out.println("[StreamBuffer][OutputStream][write] " + System.nanoTime());
+            } while (written < len && buffer.writeOpened.get());
+
+            this.buffer.lock.notifyAll();
+        }
+        //System.out.println("[StreamBuffer][BufferProvider][write] Wrote " + len + " bytes");
     }
 
     @Override
@@ -175,6 +302,10 @@ class BufferProvider extends OutputStream {
         synchronized (buffer.lock) {
             buffer.lock.notifyAll();
         }
-       System.out.println("[StreamBuffer][OutputStream][close] " + System.nanoTime());
+    }
+
+    @Override
+    public void flush() {
+        //System.out.println("[StreamBuffer][BufferProvider][flush] Flush!");
     }
 }
