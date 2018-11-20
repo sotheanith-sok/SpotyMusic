7289385e304def59be661efd1047ccc8afd327fe
Sotheanith Sok <thetheanith@gmail.com>
2018-11-19 23:51:43 -0800

    add customise context menu

---
+++ src/ui/controller/SongsContextMenu.java
@@ -0,0 +1,54 @@
+package ui.controller;
+
+import connect.Playlist;
+import connect.Song;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.scene.control.ContextMenu;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuItem;
+
+public class SongsContextMenu extends ContextMenu {
+
+   private ObservableList<Playlist> playlists;
+   private Menu adder;
+   private MenuItem remover;
+
+   private Song song;
+   public SongsContextMenu(Song song, ObservableList<Playlist> playlists){
+      super();
+      this.playlists=FXCollections.observableArrayList();
+      adder=new Menu();
+      adder.setText("Add to...");
+      remover=new MenuItem("Delete");
+      this.getItems().addAll(adder,remover);
+      remover.setOnAction(event -> {
+         removeFromPlaylist((Playlist) ((MenuItem) event.getSource()).getUserData());
+      });
+      this.song = song;
+      this.playlists=playlists;
+      updateAdder();
+   }
+
+
+   public void addToPlaylist(Playlist playlist){
+      playlist.addSong(song);
+   }
+
+   public void removeFromPlaylist(Playlist playlist){
+      playlist.removeSong(song);
+   }
+
+   private void updateAdder(){
+      adder.getItems().clear();
+      for(Playlist playlist : playlists){
+         MenuItem item = new MenuItem();
+         item.setText(playlist.getName());
+         item.setUserData(playlist);
+         adder.getItems().add(item);
+         item.setOnAction(event -> {
+            addToPlaylist((Playlist) ((MenuItem) event.getSource()).getUserData());
+         });
+      }
+   }
+}
