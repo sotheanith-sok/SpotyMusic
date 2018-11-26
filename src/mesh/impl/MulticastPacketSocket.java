2062ab88b2f41b4bbac79ba56108960510c59fde
NicholasUtz <nicholas.utz@student.csulb.edu>
2018-11-26 13:03:24 -0800

    Minor changes to MulticastPacketSocket

--- src/mesh/impl/MulticastPacketSocket.java
+++ src/mesh/impl/MulticastPacketSocket.java
@@ -39,7 +39,7 @@ public class MulticastPacketSocket {
         this.multicastSocket = new MulticastSocket(Utils.getSocketAddress(address.getPort()));
         this.multicastSocket.setSoTimeout((int) Constants.RESEND_DELAY / 2);
         this.multicastSocket.joinGroup(address.getAddress());
-        this.multicastSocket.setLoopbackMode(true);
+        //this.multicastSocket.setLoopbackMode(true);
         System.out.println("[MulticastPacketSocket] MulticastSocket localAddress: " + this.multicastSocket.getLocalAddress() + ":" + this.multicastSocket.getLocalPort() + " loopbackMode=" + this.multicastSocket.getLoopbackMode());
         this.lock = new Object();
         this.handlers = new ConcurrentHashMap<>();
@@ -50,13 +50,15 @@ public class MulticastPacketSocket {
     private void receiver() {
         DatagramPacket temp = new DatagramPacket(new byte[Constants.PACKET_SIZE], Constants.PACKET_SIZE);
 
+        System.out.println("[MulticastPacketSocket][receiver] Receiver thread starting");
+
         while (this.running.get()) {
             try {
                 //System.out.println("[MulticastPacketSocket][receiver][FINEST] Listening for packet on multicast socket");
                 this.multicastSocket.receive(temp);
-                if (temp.getSocketAddress().equals(this.multicastSocket.getLocalSocketAddress())) continue;
                 InetAddress address = temp.getAddress();
                 System.out.println("[MulticastPacketSocket][receiver][FINER] Received packet from: " + address + ":" + temp.getPort());
+                if (address.equals(this.multicastSocket.getLocalSocketAddress())) continue;
 
                 ByteArrayInputStream instrm = new ByteArrayInputStream(temp.getData(), temp.getOffset(), temp.getLength());
                 JsonStreamParser parser = new JsonStreamParser(instrm, true, (field) -> {
