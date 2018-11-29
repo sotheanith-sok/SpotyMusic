58173ea7ac1a530d277e7a383a7fc5063aaa2182
NicholasUtz <nicholas.utz@student.csulb.edu>
2018-11-28 16:43:50 -0800

    Increased Socketplexer log filter level to clear up log clutter.

--- src/net/reqres/Socketplexer.java
+++ src/net/reqres/Socketplexer.java
@@ -56,7 +56,7 @@ public class Socketplexer {
     public Socketplexer(Socket socket, ExecutorService executor) {
         this.socket = socket;
 
-        this.logger = new Logger("Socketplexer", Constants.FINE);
+        this.logger = new Logger("Socketplexer", Constants.LOG);
 
         //this.logger.trace(" Initializing channel collections");
         this.outputChannels = new HashMap<>();
